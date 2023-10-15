ebbe13d78efa56551ff8c8a9157a89c1eef2953b
Mário Mello <marioesmello@gmail.coim>
2023-10-15 19:13:42 -0300

    Iniciando repositório com instalação default

---
+++ README.md
@@ -0,0 +1,7 @@
+# FIAP - Postech
+## Coffee App
+
+### Docker
+`docker build --no-cache -t mariosmello/fiap-postech-coffee-app:latest .`
+
+`docker push mariosmello/fiap-postech-coffee-app:latest`
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,68 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cafeteria Delícia</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background: url('cafe-background.jpg') no-repeat center center/cover;
+            color: white;
+        }
+
+        header {
+            background: rgba(0, 0, 0, 0.5);
+            color: white;
+            text-align: center;
+            padding: 2em 0;
+        }
+
+        section {
+            padding: 50px 0;
+            text-align: center;
+        }
+
+        footer {
+            background: rgba(0, 0, 0, 0.5);
+            color: white;
+            text-align: center;
+            padding: 1em 0;
+        }
+    </style>
+</head>
+
+<body>
+<header>
+    <h1>Cafeteria Delícia</h1>
+    <p>O melhor lugar para os amantes de café!</p>
+</header>
+
+<section id="about">
+    <h2>Sobre Nós</h2>
+    <p>Conheça nossa cafeteria aconchegante, onde servimos os melhores grãos de café e deliciosas sobremesas.</p>
+</section>
+
+<section id="menu">
+    <h2>Menu</h2>
+    <p>Confira nosso menu variado, incluindo uma ampla seleção de cafés, chás, bolos e sanduíches.</p>
+</section>
+
+<section id="specials">
+    <h2>Ofertas Especiais</h2>
+    <p>Descubra nossas ofertas especiais da semana e promoções para os clientes fiéis.</p>
+</section>
+
+<section id="contact">
+    <h2>Contato</h2>
+    <p>Visite-nos em:</p>
+    <p>123 Rua do Café, Cidade do Café, Brasil</p>
+    <p>Email: info@cafeteriadelicia.com<br>Telefone: (123) 456-7890</p>
+</section>
+
+<footer>
+    <p>&copy; 2023 Cafeteria Delícia. Todos os direitos reservados.</p>
+</footer>
+</body>
+
+</html>
